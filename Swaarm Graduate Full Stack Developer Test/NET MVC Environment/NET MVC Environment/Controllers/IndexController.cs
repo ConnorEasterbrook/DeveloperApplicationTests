11fd5f7c44a1d33c33745072ae4ab5ab8b57bbd4
Connor <connoreaster@gmail.com>
2023-04-16 16:27:19 +0100

    Basic input [Swaarm]
    Next commit should be input being added into the database and the database being shown on index

---
+++ Swaarm Graduate Full Stack Developer Test/NET MVC Environment/NET MVC Environment/Controllers/IndexController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace NET_MVC_Environment.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContact(string name, string email)
+        {
+            // TODO: Add code to store the name and email address
+            Debug.WriteLine("\n\n" + name + " " + email + "\n\n");
+
+            return Json(new { success = true });
+        }
+    }
+}
