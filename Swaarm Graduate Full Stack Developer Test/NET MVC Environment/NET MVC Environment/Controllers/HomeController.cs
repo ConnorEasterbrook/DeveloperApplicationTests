5a1ff1fd5ee986f16b859ffe67af13024efc07c2
Connor <connoreaster@gmail.com>
2023-04-16 14:09:59 +0100

    Pre-work SQL [Swaarm]

--- Swaarm Graduate Full Stack Developer Test/NET MVC Environment/NET MVC Environment/Controllers/HomeController.cs
+++ Swaarm Graduate Full Stack Developer Test/NET MVC Environment/NET MVC Environment/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NET_MVC_Environment.Models;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 
 namespace NET_MVC_Environment.Controllers
@@ -8,14 +10,52 @@ namespace NET_MVC_Environment.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private Database _database;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
+            _database = new Database("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=\"E:\\NOT_WINDOWS\\GITHUB\\DEVELOPERAPPLICATIONTESTS\\SWAARM GRADUATE FULL STACK DEVELOPER TEST\\NET MVC ENVIRONMENT\\SWAARMTESTDB.MDF\";" +
+                "Integrated Security=True;" +
+                "Connect Timeout=30;" +
+                "Encrypt=False;" +
+                "TrustServerCertificate=False;" +
+                "ApplicationIntent=ReadWrite;" +
+                "MultiSubnetFailover=False");
         }
 
         public IActionResult Index()
         {
+            TemporaryTable();
+
             return View();
+        }
+
+        public void TemporaryTable()
+        {
+            _database.OpenConnection();
+
+            string tableName = "TestTable";
+
+            if (_database.TableExists(tableName))
+            {
+                _database.RemoveTable(tableName);
+            }
+
+            string[] columnNames = { "Id INT PRIMARY KEY", "Name VARCHAR(50)", "Email VARCHAR(80)" };
+            _database.AddTable(tableName, columnNames);
+
+            string[] rowValues = { "1", "'John Doe'", "'john.doe@example.com'" };
+            _database.AddData(tableName, rowValues);
+
+            DataRow row = _database.SelectRow(tableName, "Id = 1");
+            Debug.WriteLine("\n\n" + row["Name"] + " " + row["Email"] + "\n\n");
+
+            _database.RemoveData(tableName, "Id = 1");
+
+            _database.RemoveTable(tableName);
+            _database.CloseConnection();
         }
 
         public IActionResult Privacy()
---
+++ Swaarm Graduate Full Stack Developer Test/NET MVC Environment/NET MVC Environment/Database.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NET_MVC_Environment
+{
+    public class Database
+    {
+        private SqlConnection _connection;
+
+        public Database(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            _connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+            }
+        }
+
+        public void AddTable(string tableName, string[] columns)
+        {
+            string query = $"CREATE TABLE {tableName} ({string.Join(",", columns)})";
+            SqlCommand command = new SqlCommand(query, _connection);
+            command.ExecuteNonQuery();
+        }
+
+        public bool TableExists(string tableName)
+        {
+            string query = $"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tableName}'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, "Tables");
+            return dataSet.Tables["Tables"].Rows.Count > 0;
+        }
+
+        public void RemoveTable(string tableName)
+        {
+            string query = $"DROP TABLE {tableName}";
+            SqlCommand command = new SqlCommand(query, _connection);
+            command.ExecuteNonQuery();
+        }
+
+        public void AddData(string tableName, string[] values)
+        {
+            string query = $"INSERT INTO {tableName} VALUES ({string.Join(",", values)})";
+            SqlCommand command = new SqlCommand(query, _connection);
+            command.ExecuteNonQuery();
+        }
+
+        public DataRow SelectRow(string tableName, string condition)
+        {
+            string query = $"SELECT * FROM {tableName} WHERE {condition}";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, tableName);
+
+            if (dataSet.Tables[tableName].Rows.Count > 0)
+            {
+                return dataSet.Tables[tableName].Rows[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void RemoveData(string tableName, string condition)
+        {
+            string query = $"DELETE FROM {tableName} WHERE {condition}";
+            SqlCommand command = new SqlCommand(query, _connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
